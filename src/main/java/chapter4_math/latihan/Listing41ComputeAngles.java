7964b54e81d0ac297bdc1e3864b11b36968de37b
Ilyas Nur Salim <ilyaznoorsaliem@gmail.comgit config --global user.email ilyaznoorsaliem@gmail.com>
2023-01-12 17:51:06 +0700

    first commit

--- src/main/java/chapter4_math/latihan/Listing41ComputeAngles.java
+++
@@ -1,7 +0,0 @@
-package chapter4_math.latihan;
-
-public class Listing41ComputeAngles {
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/chapter4_math/latihan/Listing42OrderTwoCities.java
+++
@@ -1,7 +0,0 @@
-package chapter4_math.latihan;
-
-public class Listing42OrderTwoCities {
-    public static void main(String[] args) {
-
-    }
-}
